e55c9e53ae87ff340a75f28dd07dff5ca6e6d947
lee-at-zoo-corp <lee@zoo.dev>
2026-04-02 17:20:18 -0400

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
 import * as zoo from '@kittycad/lib'
 import svgZoo from './svg-zoo'
 
-const token = 'api-564d9062-9a7a-41ee-944e-db5a3dcedc2a'
-
-const zooClient = new zoo.Client({
-  token,
-  baseUrl: 'wss://api.dev.zoo.dev',
-  fetch: window.fetch
-})
-
-// TODO: LOAD WASM BLOB INTO WEB WORKER FOR PARSING INTO JSON
+// TODO: THROTTLE MOUSE EVENTS TO 30 EPS
+// TODO: "TURN OFF" ALL OTHER STREAMS WHEN A NEW ONE IS STARTED
 
 type Size = {
   width: number,
@@ -17,10 +10,9 @@ type Size = {
 }
 
 const zooWebViewDecoOff = (size: Size, elZooWebView: HTMLDivElement) => {
-  elZooWebView.style.width = size.width
-  elZooWebView.style.height = size.height
+  elZooWebView.style.width = size.width.toString()
+  elZooWebView.style.height = size.height.toString()
   
-  elZooWebView.style.display = 'flex'
   elZooWebView.style.justifyContent = 'center'
   elZooWebView.style.alignItems = 'center'
   elZooWebView.style.cursor = 'pointer'
@@ -30,27 +22,27 @@ const zooWebViewDecoOff = (size: Size, elZooWebView: HTMLDivElement) => {
 const zooWebViewDecoOn = (size: Size, elZooWebView: HTMLDivElement, elStart: HTMLDivElement) => {
   elZooWebView.style.justifyContent = 'right'
   elZooWebView.style.alignItems = 'flex-start'
-  elStart.style.width = size.width / 4
+  elStart.style.width = (size.width / 4).toString()
   elStart.style.color = 'hsl(154deg 100% 58%)'
   elStart.style.paddingTop = '2px'
   elStart.style.paddingRight = '2px'
 }
 
-const createZooWebView = (args: { size: Size }) => {
+const createZooWebViewHTML = (args: { size: Size }) => {
   const elZooWebView = document.createElement('div')
   const elVideo = document.createElement('video')
   const elStart = document.createElement('div')
+  
   elStart.classList.add('start')
   elStart.innerHTML = svgZoo
-  elStart.style.width = args.size.width / 2
+  elStart.style.width = (args.size.width / 2).toString()
   elStart.style.position = 'absolute'
   elStart.style.color = 'hsl(154deg 100% 25%)'
   
   elVideo.width = args.size.width - args.size.width % 4
   elVideo.height = args.size.height - args.size.height % 4
-  elVideo.addEventListener('contextmenu', (event) => {
-    event.preventDefault()
-  })
+  
+  elZooWebView.style.display = 'flex'
 
   zooWebViewDecoOff(args.size, elZooWebView)
     
@@ -59,48 +51,49 @@ const createZooWebView = (args: { size: Size }) => {
   return elZooWebView
 }
 
-    
-document.addEventListener('DOMContentLoaded', () => {
-  const width = 256
-  const height = 256
-  
-  // Make sure no other ZooWebView are running.
-  // window.zooWebViews.forEach((view) => {
-  //   view.deconstructor()
-  // })
-  
-  const size: Size = {
-    width: width - width % 4,
-    height: height - height % 4,
+const preventDefault = (e: Event) => e.preventDefault()
+
+const createZooWebView = (args: {
+  zooClient: zoo.Client,
+  size: Size,
+  onReady: () => void,
+}) => {
+  const sizeAdjusted: Size = {
+    width: args.size.width - args.size.width % 4,
+    height: args.size.height - args.size.height % 4,
   }
   
-  const elZooWebView = createZooWebView({ size })
-  const elVideo = elZooWebView.querySelector('video')
-  const elStart = elZooWebView.querySelector('div.start')
-
-  elStart.addEventListener('click', () => {
-    zooWebViewDecoOn(size, elZooWebView, elStart)
+  const elZooWebView = createZooWebViewHTML({ size: sizeAdjusted })
   
+  const elVideo = elZooWebView.querySelector('video')
+  if (elVideo === null) return
+  elVideo.addEventListener('contextmenu', preventDefault)
+ 
+  const elStart = elZooWebView.querySelector('div.start')
+  if (elStart === null) return
+  
+  const promiseWebRTC = Promise.withResolvers()
+  const startZooWebRTC = () => {
     // Owns setting up the WebSocket. Because the WebSocket is only good for a
     // single WebRTC handshake, and it's to be used as the ICE information
     // exchange, any other usage by an application is unexpected.
     const zooWebRTC = new zoo.WebRTC({
-      client: zooClient,
-      video_res_width: size.width,
-      video_res_height: size.height,
+      client: args.zooClient,
+      video_res_width: sizeAdjusted.width,
+      video_res_height: sizeAdjusted.height,
       show_grid: true,
       post_effect: 'ssao',
       fps: 30,
     })
 
-    // zooWebRTC.addEventListener('open', ...)
-
-    zooWebRTC.addEventListener('track', (event) => {
+    const onTrack = (event: Event) => {
       elVideo.srcObject = event.target.track?.streams[0]
-    })
+    }
+    zooWebRTC.addEventListener('track', onTrack, { once: true })
     
-    zooWebRTC.addEventListener('connected', (event) => {
-      void elVideo.play()
+    const onConnected = (event: Event) => {
+      void elVideo.play().catch(console.warn)
+      
       zooWebRTC.send(JSON.stringify({
         "type": "modeling_cmd_batch_req",
         "requests": [
@@ -147,13 +140,70 @@ document.addEventListener('DOMContentLoaded', () => {
         "batch_id": "00000000-0000-0000-0000-000000000000",
         "responses": true
       }))
-    })
-
-    // zooWebRTC.addEventListener('error', ...)
-    // zooWebRTC.addEventListener('close', ...)
+      
+      promiseWebRTC.resolve(zooWebRTC)
+    }
     
-    const removeMouseEvents = zooWebRTC.addMouseEvents(elVideo)
+    zooWebRTC.addMouseEvents(elVideo)
+    zooWebRTC.addEventListener('connected', onConnected, { once: true })
+    
+    zooWebRTC.start()
+  }
+  
+  const elStartClick = () => {
+    zooWebViewDecoOn(sizeAdjusted, elZooWebView, elStart)
+    startZooWebRTC()
+  }
+  elStart.addEventListener('click', elStartClick)
+  
+  return {
+    el: elZooWebView,
+    rtc: promiseWebRTC.promise,
+    deconstructor: () => {
+      elStart.removeEventListener('click', elStartClick)
+      elVideo.removeEventListener('contextmenu', preventDefault)
+      return Promise.allSettled([
+        promiseWebRTC.promise.then((rtc) => rtc.deconstructor())
+      ])
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const token = 'api-564d9062-9a7a-41ee-944e-db5a3dcedc2a'
+
+  const zooClient = new zoo.Client({
+    token,
+    baseUrl: 'wss://api.dev.zoo.dev',
   })
- 
-  document.body.appendChild(elZooWebView)
+  
+  new Array(9).fill(0).forEach((_e, idx) => {
+    const zooWebView = createZooWebView({
+      zooClient,
+      size: {
+        width: 256,
+        height: 256,
+      },
+    })
+    document.body.appendChild(zooWebView.el)
+    
+    zooWebView.rtc.then(async (rtc) => {
+      const cmds = await rtc.prepareToExecuteKcl(`
+        sketch001 = startSketchOn(XY)
+        profile001 = startProfile(sketch001, at = [-3.5, -2.23])
+          |> line(end = [4.53, 5.73])
+          |> line(end = [5.18, -3.74])
+          |> line(endAbsolute = [profileStartX(%), profileStartY(%)])
+          |> close()
+        extrude001 = extrude(profile001, length = 5 * ${idx})
+      `)
+      // There's also stream.removeEventListener which should be
+      // called when no more events.
+      cmds.addEventListener((ev) => {
+        // receieve responses in ev.data
+        console.log(ev.data)
+      })
+      cmds.start()
+    })
+  })
 })
